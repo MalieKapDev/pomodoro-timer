f95a39ba8ef29a7a1dd0e59361b4e812bdee6b32
Malie Kapelianis <maliekapelianis@gmail.com>
2024-11-16 12:46:13 +0200

    Installed bootstrap javascript npm. Updated favicons to correct href.  Created logoContainer component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
+import LogoContainer from "./logoContainer";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello from React!</h1>
-      </header>
+    <div className="container-fluid main-container ps-5 pe-5">
+      <LogoContainer />
     </div>
   );
 }
